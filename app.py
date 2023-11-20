ee13c941d2caf2cf57c9ad7293e9882ec7ffb7a3
aliagha135 <aliagha135@hotmail.com>
2023-11-20 17:11:50 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,391 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+import re
+import datetime
+from openpyxl.utils.dataframe import dataframe_to_rows
+import os
+
+st.set_page_config(layout="wide")
+
+
+def get_file_path(date):
+    return date.strftime("%d-%m-%Y") + '.xlsx'
+
+def extract_numeric(value):
+    # Extract numeric part from the string using regular expression
+    match = re.search(r'\d+(\.\d+)?', str(value))
+    return float(match.group()) if match else 0.0
+
+def parse_message(message, selected_rider):
+    data = {
+        "Name": "",
+        "Phone": "",
+        "Delivery Address": "",
+        "Fare": "",
+        "Fare paid Online": "",  # New field
+        "Cash": "",
+        "Online": "",
+        "Credit Card": "",
+        "last-digits": "",
+        "Rider": selected_rider.lower()  # Use the selected rider
+    }
+
+    # Split the message by lines and process each line
+    lines = message.split('\n')
+    for line in lines:
+        if "Name:" in line:
+            data["Name"] = line.replace("Name:", "").strip().lower()
+        elif "Phone:" in line:
+            data["Phone"] = line.replace("Phone:", "").strip().replace(" ", "").replace("-", "")
+        elif "Delivery Address:" in line:
+            data["Delivery Address"] = line.replace("Delivery Address:", "").strip().lower()
+        elif "Fare:" in line:
+            data["Fare"] = extract_numeric(line.replace("Fare:", "").strip())
+        elif "Fare paid Online:" in line:  # Handle Fare paid Online
+            data["Fare paid Online"] = line.replace("Fare paid Online:", "").strip().lower()
+        elif "Cash:" in line:
+            data["Cash"] = extract_numeric(line.replace("Cash:", "").strip())
+        elif "Online:" in line:
+            data["Online"] = extract_numeric(line.replace("Online:", "").strip())
+        elif "Credit Card:" in line:
+            data["Credit Card"] = extract_numeric(line.replace("Credit Card:", "").strip())
+        elif "last-digits:" in line:  # Handling the last-digits field
+            data["last-digits"] = line.replace("last-digits:", "").strip()
+        # elif "Rider:" in line:
+        #     data["Rider"] = line.replace("Rider:", "").strip().lower()
+
+    return data
+
+
+
+
+
+def set_column_widths(worksheet, widths):
+    for i, column_width in enumerate(widths, start=1):
+        worksheet.column_dimensions[openpyxl.utils.get_column_letter(i)].width = column_width
+
+
+def write_to_spreadsheet(data, file_path):
+    if not os.path.exists(file_path):
+        df = pd.DataFrame(columns=['Name', 'Phone', 'Delivery Address', 'Fare', 'Fare paid Online', 'Cash', 'Online', 'Credit Card', 'Rider'])
+        df.to_excel(file_path, index=False)
+
+    try:
+        # Read the existing file, ensuring 'Phone' is read as a string
+        df = pd.read_excel(file_path, dtype={'Phone': str})
+
+        # Prepare new data ensuring 'Phone' is a string
+        new_data_df = pd.DataFrame([data])
+        new_data_df['Phone'] = new_data_df['Phone'].astype(str)
+
+        # Concatenate and save
+        df = pd.concat([df, new_data_df], ignore_index=True)
+        df.to_excel(file_path, index=False)
+    except Exception as e:
+        st.error(f"Error processing file: {e}")
+
+def display_by_rider(df, rider_name):
+    filtered_df = df[df['Rider'] == rider_name]
+    if not filtered_df.empty:
+        st.markdown(f"<h3 style='color:#5C83F5;'>{rider_name.capitalize()}</h3>", unsafe_allow_html=True)
+        st.dataframe(filtered_df)
+
+        # Function to extract numeric part from the string
+        def extract_numeric(value):
+            match = re.search(r'\d+(\.\d+)?', str(value))
+            return float(match.group()) if match else 0.0
+
+        # Calculate sums
+        sum_fare = sum(filtered_df['Fare'].apply(extract_numeric))
+        sum_cash = sum(filtered_df['Cash'].apply(extract_numeric))
+        sum_online = sum(filtered_df['Online'].apply(extract_numeric))
+        sum_credit = sum(filtered_df['Credit Card'].apply(extract_numeric))
+
+        # Display metrics
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric("Total Fare", f"{sum_fare}")
+        col2.metric("Total Cash", f"{sum_cash}")
+        col3.metric("Total Online", f"{sum_online}")
+        col4.metric("Total Credit Card", f"{sum_credit}")
+
+        st.markdown("---")
+
+def display_stats_view(df):
+    col1, s1, col2, s2, col3, s3, col4, s4, col5 = st.columns([3, 1, 3, 1, 4, 1, 3, 1, 4])
+
+    # Specific table for Pickup
+    pickup_df = df[df['Delivery Address'].str.lower().isin(['pickup', 'pick-up'])]
+    pickup_cash_total = pickup_df['Cash'].sum()
+    pickup_online_total = pickup_df['Online'].sum()
+    pickup_credit_card_total = pickup_df['Credit Card'].sum()
+
+    # Append pickup totals to each respective total dataframe
+    pickup_totals_entry = pd.DataFrame([{'Rider': 'Pickup', 'Cash': pickup_cash_total, 'Online': pickup_online_total, 'Credit Card': pickup_credit_card_total}])
+
+    # Table for Cash Totals by Rider
+    with col1:
+        cash_total = df.groupby('Rider')['Cash'].sum().reset_index()
+        cash_total = pd.concat([cash_total, pickup_totals_entry[['Rider', 'Cash']]], ignore_index=True)
+        st.markdown(f"<h3 style='color:#5C83F5;'>Cash</h3>", unsafe_allow_html=True)
+        st.dataframe(cash_total)
+        total_cash = cash_total['Cash'].sum()
+        st.metric("Total Cash", f"{total_cash} Rs")
+        st.markdown("---")
+
+    # Table for Online Totals by Rider
+    with col2:
+        online_total = df.groupby('Rider')['Online'].sum().reset_index()
+        online_total = pd.concat([online_total, pickup_totals_entry[['Rider', 'Online']]], ignore_index=True)
+        st.markdown(f"<h3 style='color:#5C83F5;'>Online</h3>", unsafe_allow_html=True)
+        st.dataframe(online_total)
+        total_online = online_total['Online'].sum()
+        st.metric("Total Online", f"{total_online} Rs")
+        st.markdown("---")
+
+    # Table for Credit Card Totals by Name, including last-digits as the last column
+    with col3:
+        card_total = df.groupby(['Name', 'last-digits'])['Credit Card'].sum().reset_index()
+        st.markdown(f"<h3 style='color:#5C83F5;'>Card Payments</h3>", unsafe_allow_html=True)
+        st.dataframe(card_total)
+        total_credit_card = card_total['Credit Card'].sum()
+        st.metric("Total Credit Card", f"{total_credit_card} Rs")
+        st.markdown("---")
+
+    with col4:
+        st.markdown(f"<h3 style='color:#5C83F5;'>Online Payments</h3>", unsafe_allow_html=True)
+        online_payments_df = df[df['Online'] > 0][['Name', 'Online']]
+        st.dataframe(online_payments_df)
+        total_online_transfer = online_payments_df['Online'].sum()
+        st.metric("Total Online", f"{total_online_transfer} Rs")
+        st.markdown("---")
+
+    # Specific table for Pickup
+    with col5:
+        st.markdown(f"<h3 style='color:#5C83F5;'>Pickup</h3>", unsafe_allow_html=True)
+        st.dataframe(pickup_df[['Name', 'Cash', 'Online', 'Credit Card']])
+        c1, c2, c3 = st.columns(3)
+        c1.metric("Pickup Cash", f"{pickup_cash_total} Rs")
+        c2.metric("Pickup Online", f"{pickup_online_total} Rs")
+        c3.metric("Pickup Credit Card", f"{pickup_credit_card_total} Rs")
+        st.markdown("---")
+
+
+def display_daily_balance(df):
+    col1,col2 = st.columns(2)
+    with col1:
+
+        st.markdown("### Daily Balance")
+
+        # Initialize the balance DataFrame with opening balance
+        balance_data = [{
+            "Name": "Opening Balance",
+            "Running Balance (Cash)": 7000,
+            "Cash In": 0,
+            "Cash Out": 0,
+            "Online": 0
+        }]
+
+        # Initialize the running balance
+        running_balance = 7000
+
+        for index, row in df.iterrows():
+            cash_in = row['Cash'] if row['Cash'] else 0
+            cash_out = 0
+            online = row['Online'] if row['Online'] else 0
+            fare_paid_online = row.get('Fare paid Online', 'no') == 'yes'
+            fare_amount = row['Fare'] if row['Fare'] else 0
+
+            # Add 'Fare' prefix to the name if fare is paid online and needs to be subtracted
+            name = row['Name'] + " Fare" if fare_paid_online else row['Name']
+
+            # Deduct fare from cash if fare is paid online
+            if fare_paid_online:
+                cash_out = -fare_amount
+                running_balance -= fare_amount
+
+            # Update running balance for cash in transactions
+            running_balance += cash_in
+
+            balance_data.append({
+                "Name": name,
+                "Running Balance (Cash)": running_balance,
+                "Cash In": cash_in,
+                "Cash Out": cash_out,
+                "Online": online
+            })
+
+        # Create a DataFrame from the balance data
+        balance_df = pd.DataFrame(balance_data)
+
+        # Calculate totals
+        total_running_balance_cash = balance_df['Running Balance (Cash)'].iloc[-1]
+        total_cash_in = balance_df['Cash In'].sum()
+        total_cash_out = balance_df['Cash Out'].sum()
+        total_online = balance_df['Online'].sum()
+
+        # Create a DataFrame for the totals row
+        totals_df = pd.DataFrame([{
+            "Name": "Totals",
+            "Running Balance (Cash)": total_running_balance_cash,
+            "Cash In": total_cash_in,
+            "Cash Out": total_cash_out,
+            "Online": total_online
+        }])
+
+        # Concatenate the totals DataFrame to the original DataFrame
+        balance_df = pd.concat([balance_df, totals_df], ignore_index=True)
+
+        total_running_balance_cash = balance_df['Running Balance (Cash)'].iloc[-1]
+        total_cash_in = balance_df['Cash In'].sum()
+        total_cash_out = balance_df['Cash Out'].sum()
+        total_online = balance_df['Online'].sum()
+
+        # Display the DataFrame
+        st.dataframe(balance_df)
+
+        # Display totals using metrics
+        st.markdown("### Totals")
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric("Total Running Balance (Cash)", f"{total_running_balance_cash}")
+        col2.metric("Total Cash In", f"{total_cash_in}")
+        col3.metric("Total Cash Out", f"{total_cash_out}")
+        col4.metric("Total Online", f"{total_online}")
+
+def main_app():
+    st.title("Crumble Data Entry App")
+
+    today = datetime.datetime.now().strftime("%d-%m-%Y")
+    file_path = f'{today}.xlsx'
+
+
+    default_message = '''Name:
+    Phone:
+    Delivery Address:
+    Fare:
+    Cash:
+    Online:
+    Credit Card:
+    last-digits:'''
+
+    if 'message_text' not in st.session_state or st.session_state.message_text.strip() == "":
+        st.session_state.message_text = default_message
+
+    view_option = st.radio("Select View", ("Main View", "Rider View", "Stats View"), horizontal=True)
+    col1, spacer, col2 = st.columns([4, 1, 5])
+
+
+    if view_option == "Main View":
+        with col1:
+            st.session_state.message_text = st.text_area("Enter the message here:", st.session_state.message_text, height=300)
+            selected_rider = st.radio("Select Rider", ["Pickup", "Shazaib", "Zubair", "Indrive"], horizontal=True)
+            fare_paid_online_option = st.radio("Fare Paid Online", ["No", "Yes"], horizontal=True)
+
+            if st.button("Process", key='process1'):
+                data = parse_message(st.session_state.message_text, selected_rider)
+                data['Fare paid Online'] = fare_paid_online_option.lower()
+
+                # Check for incomplete fields
+                required_fields = [str(data[field]) for field in ['Name', 'Phone', 'Delivery Address', 'Fare', 'last-digits']]
+                if any(field.strip() == "" for field in required_fields):
+                    st.error("Error: Please fill all required fields ( and 1 from among Cash, Online, and Credit Card).")
+                else:
+                    # Check if Cash, Online, and Credit Card fields have valid single entry
+                    payment_methods = [data['Cash'], data['Online'], data['Credit Card']]
+                    filled_methods = sum([1 for method in payment_methods if method not in [None, "", 0]])
+
+                    if filled_methods > 1:
+                        st.error("Error: Only one of Cash, Online, or Credit Card can have a value at a time.")
+                    elif data['Cash'] not in [None, "", 0] and data['Fare paid Online'] == 'yes':
+                        st.error("Error: Cannot process as Cash value is present and Fare Paid Online is marked as 'Yes'.")
+                    else:
+                        today = datetime.datetime.now().strftime("%d-%m-%Y")
+                        file_path = f'{today}.xlsx'
+                        write_to_spreadsheet(data, file_path)
+                        st.success("Data added to spreadsheet.")
+
+            delete_record_num = st.number_input("Enter record number to delete", min_value=0, step=1) + 1
+            if st.button("Delete Record", key='delete1'):
+                if os.path.exists(file_path):
+                    try:
+                        df = pd.read_excel(file_path)
+                        if 1 <= delete_record_num <= len(df):
+                            if delete_record_num > len(df) - 10:
+                                df = df.drop(df.index[delete_record_num - 1])
+                                df.to_excel(file_path, index=False)
+                                st.success(f"Record number {delete_record_num - 1} deleted.")
+                            else:
+                                st.error("Only the last 10 records can be deleted.")
+                        else:
+                            st.error("Record number out of range.")
+                    except Exception as e:
+                        st.error(f"An error occurred: {e}")
+
+        with col2:
+            if os.path.exists(file_path):
+                df = pd.read_excel(file_path, dtype={'Phone': str})
+                # Convert the columns to string for display
+                df['Phone'] = df['Phone'].astype(str).str.replace(r'\.0$', '', regex=True)
+                df['Cash'] = df['Cash'].astype(str)
+                df['Online'] = df['Online'].astype(str)
+                df['Credit Card'] = df['Credit Card'].astype(str)
+
+                st.write("Last 10 Records:")
+                st.dataframe(df.tail(10))
+
+    elif view_option == "Rider View":
+        col_date, _ = st.columns([1, 14]) 
+        col_err, _ = st.columns([1, 5])
+        with col_date:
+            selected_date = st.date_input("Select Date", datetime.datetime.today())
+        file_path_for_date = selected_date.strftime("%d-%m-%Y") + '.xlsx'
+        col1, spacer, col2 = st.columns([4, 1, 4])
+        if os.path.exists(file_path_for_date):
+            df = pd.read_excel(file_path_for_date, dtype={'Phone': str, 'Fare': float, 'Cash': float, 'Online': float, 'Credit Card': float})
+            unique_riders = df['Rider'].unique()
+            for i, rider in enumerate(unique_riders):
+                with (col1 if i % 2 == 0 else col2):
+                    display_by_rider(df, rider)
+        else:
+            with col_err:
+                st.error(f"No records found for {selected_date.strftime('%d-%m-%Y')}")
+
+    elif view_option == "Stats View":
+        col_date, _ = st.columns([1, 14]) 
+        col_err, _ = st.columns([1, 5])
+        with col_date:
+            selected_date = st.date_input("Select Date", datetime.datetime.today())
+        file_path_for_date = selected_date.strftime("%d-%m-%Y") + '.xlsx'
+        if os.path.exists(file_path_for_date):
+            df = pd.read_excel(file_path_for_date)
+            display_stats_view(df)
+            display_daily_balance(df)
+        else:
+            with col_err:
+                st.error(f"No records found for {selected_date.strftime('%d-%m-%Y')}")
+
+
+
+USERNAME = "crumble"
+PASSWORD = "drakoo123"
+
+def login(username, password):
+    return username == USERNAME and password == PASSWORD
+
+
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+
+
+if not st.session_state.logged_in:
+    st.sidebar.title("Login")
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+    if st.sidebar.button("Login"):
+        if login(username, password):
+            st.session_state.logged_in = True
+            st.experimental_rerun()  # Rerun the app to display the main app
+        else:
+            st.sidebar.error("Incorrect username or password.")
+else:
+    main_app()
